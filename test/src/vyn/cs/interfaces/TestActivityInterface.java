95e0ce31c5b7ffc78242d7251ebb589bfd23994e
indoallcode <msofyancs@yahoo.com>
2012-03-14 09:41:11 +0700

    Update test/src/vyn/cs/interfaces/TestActivityInterface.java

--- test/src/vyn/cs/interfaces/TestActivityInterface.java
+++ test/src/vyn/cs/interfaces/TestActivityInterface.java
@@ -1,3 +1,14 @@
+/**
+ * Test Activity Interface - Chat-Android
+ *
+ * interface to connect to server
+ *
+ * @author    M Sofiyan
+ * @email     msofyancs@gmail.com
+ * @skypeid   viyancs
+ * please don't remove this comment if you want to using part of full this code
+ * 
+ **/
 package vyn.cs.interfaces;
 
 import vyn.socket.IOSocket;

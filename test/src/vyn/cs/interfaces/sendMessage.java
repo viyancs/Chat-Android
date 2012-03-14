7ff436e556b048841c061d94bb3c5956d9168396
indoallcode <msofyancs@yahoo.com>
2012-03-14 09:40:12 +0700

    Update test/src/vyn/cs/interfaces/sendMessage.java

--- test/src/vyn/cs/interfaces/sendMessage.java
+++ test/src/vyn/cs/interfaces/sendMessage.java
@@ -1,3 +1,14 @@
+/**
+ * Send Message - Chat-Android
+ *
+ * interface to addlayout when send and receive message from server
+ *
+ * @author    M Sofiyan
+ * @email     msofyancs@gmail.com
+ * @skypeid   viyancs
+ * please don't remove this comment if you want to using part of full this code
+ * 
+ **/
 package vyn.cs.interfaces;
 
 import android.view.ViewGroup.LayoutParams;

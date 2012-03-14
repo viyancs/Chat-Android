3df405945a018ac02626c20d2a1c43804e4064a7
indoallcode <msofyancs@yahoo.com>
2012-03-14 09:43:02 +0700

    Update test/src/vyn/json/online/ResponseRoot.java

--- test/src/vyn/json/online/ResponseRoot.java
+++ test/src/vyn/json/online/ResponseRoot.java
@@ -1,3 +1,14 @@
+/**
+ * Response Root - Chat-Android
+ *
+ * class to parsing json users who has been online from server
+ *
+ * @author    M Sofiyan
+ * @email     msofyancs@gmail.com
+ * @skypeid   viyancs
+ * please don't remove this comment if you want to using part of full this code
+ * 
+ **/
 package vyn.json.online;
 
 import java.util.List;

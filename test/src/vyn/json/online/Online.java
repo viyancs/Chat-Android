6c92f3417279d72290519068f26feb6f7b6398f8
indoallcode <msofyancs@yahoo.com>
2012-03-14 09:42:12 +0700

    Update test/src/vyn/json/online/Online.java

--- test/src/vyn/json/online/Online.java
+++ test/src/vyn/json/online/Online.java
@@ -1,3 +1,14 @@
+/**
+ * Online - Chat-Android
+ *
+ * class to parse json users who has been online from server 
+ *
+ * @author    M Sofiyan
+ * @email     msofyancs@gmail.com
+ * @skypeid   viyancs
+ * please don't remove this comment if you want to using part of full this code
+ * 
+ **/
 package vyn.json.online;
 import com.google.gson.annotations.SerializedName;
 public class Online {

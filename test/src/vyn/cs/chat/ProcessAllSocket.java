8ee20de1ee471c3ba03b9eda0561913a9f0faed2
indoallcode <msofyancs@yahoo.com>
2012-03-14 09:37:23 +0700

    Update test/src/vyn/cs/chat/ProcessAllSocket.java

--- test/src/vyn/cs/chat/ProcessAllSocket.java
+++ test/src/vyn/cs/chat/ProcessAllSocket.java
@@ -1,3 +1,17 @@
+/**
+ * Process All Socket - Chat-Android
+ *
+ * connect to server socket io
+ * handling response message from server
+ * request message to server
+ * update ui android to another activity
+ *
+ * @author    M Sofiyan
+ * @email     msofyancs@gmail.com
+ * @skypeid   viyancs
+ * please don't remove this comment if you want to using part of full this code
+ * 
+ **/
 package vyn.cs.chat;
 
 import java.io.IOException;

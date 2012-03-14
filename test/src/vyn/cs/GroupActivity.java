3ce347f7c512a51d2ac0bf0eb8a65496d818c541
indoallcode <msofyancs@yahoo.com>
2012-03-14 09:28:52 +0700

    Update test/src/vyn/cs/GroupActivity.java

--- test/src/vyn/cs/GroupActivity.java
+++ test/src/vyn/cs/GroupActivity.java
@@ -1,3 +1,15 @@
+/**
+ * Group Activity - Chat-Android
+ *
+ * invite users //not implemented
+ * send broadcast message to all user in one group //not implemented
+ *
+ * @author    M Sofiyan
+ * @email     msofyancs@gmail.com
+ * @skypeid   viyancs
+ * please don't remove this comment if you want to using part of full this code
+ * 
+ **/
 package vyn.cs;
 
 import android.app.Activity;
